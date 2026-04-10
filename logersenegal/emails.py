3489de1b093647337ecd84acb3e8f3936e99686c
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 06:38:59 +0000

    Email: Added BCC admin copy for non-sensitive notifications

--- logersenegal/emails.py
+++ logersenegal/emails.py
@@ -3,8 +3,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 
-def send_html_email(subject, template_name, context, to_email):
-    """Envoie un e-mail HTML stylisé avec Fallback texte."""
+def send_html_email(subject, template_name, context, to_email, bcc_admin=True):
+    """Envoie un e-mail HTML stylisé avec Fallback texte et copie cachée admin."""
     if not to_email:
         return False
         
@@ -12,11 +12,14 @@ def send_html_email(subject, template_name, context, to_email):
     html_content = render_to_string(template_name, context)
     text_content = strip_tags(html_content)
     
+    bcc = [settings.DEFAULT_FROM_EMAIL] if bcc_admin else []
+    
     email = EmailMultiAlternatives(
         subject,
         text_content,
         settings.DEFAULT_FROM_EMAIL,
-        [to_email]
+        [to_email],
+        bcc=bcc
     )
     email.attach_alternative(html_content, "text/html")
     try:
@@ -26,12 +29,13 @@ def send_html_email(subject, template_name, context, to_email):
         return False
 
 def send_otp_email(user, otp_code):
-    """Envoie le code OTP par email."""
+    """Envoie le code OTP par email (Pas de copie admin pour la confidentialité)."""
     return send_html_email(
         "Votre code de vérification - Loger Sénégal",
         "emails/otp_email.html",
         {'user': user, 'otp_code': otp_code},
-        user.email
+        user.email,
+        bcc_admin=False
     )
 
 def send_property_published_email(user, property):

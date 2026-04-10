e96ab5e2b7430a3f8fd576b7f29b8ea0a67d19e0
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 18:01:33 +0000

    Fix: Forcer la création des dossiers media et static

--- logersenegal/settings.py
+++ logersenegal/settings.py
@@ -159,10 +159,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Sécurité pour O2switch : S'assurer que les dossiers existent
+import os
+os.makedirs(STATIC_ROOT, exist_ok=True)
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 # Optimisation WhiteNoise (Version souple pour éviter l'erreur 500)
 STORAGES = {

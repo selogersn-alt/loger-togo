bf05608b0d2fe1cadeb8b5f2be9e0482cf1d0b52
selogersn@gmail.com <selogersn@gmail.com>
2026-04-10 12:05:37 +0000

    Final Polish: Enable WhiteNoise for stable static files and admin CSS

--- logersenegal/settings.py
+++ logersenegal/settings.py
@@ -70,12 +70,12 @@ AUTH_USER_MODEL = 'users.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'logersenegal.urls'
@@ -161,7 +161,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email Configuration (O2switch SMTP gratuit)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

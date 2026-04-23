10eb832edda24d52f9ab24542a02ed8a606079f0
selogersn@gmail.com <selogersn@gmail.com>
2026-04-23 01:22:34 +0000

    Add whitenoise middleware for production static files

--- logertogo/settings.py
+++ logertogo/settings.py
@@ -102,6 +102,7 @@ AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

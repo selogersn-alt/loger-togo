d6dc932ad10bc21a811fe407db2fab11d250968b
selogersn@gmail.com <selogersn@gmail.com>
2026-05-07 21:49:25 +0000

    🚀 Fix: Global i18n builtins to resolve TemplateSyntaxError

--- logertogo/settings.py
+++ logertogo/settings.py
@@ -136,6 +136,7 @@ TEMPLATES = [
             'builtins': [
                 'logersn.templatetags.property_tags',
                 'django.contrib.humanize.templatetags.humanize',
+                'django.templatetags.i18n',
             ],
         },
     },

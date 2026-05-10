adaebc42ddf74075ee1f0761664645208697c108
selogersn@gmail.com <selogersn@gmail.com>
2026-05-10 01:25:08 +0000

    Cleanup: Remove typo domains from ALLOWED_HOSTS

--- logertogo/settings.py
+++ logertogo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-for-local-de
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['157.180.127.70', 'logertg.com', 'www.logertg.com', 'logertogo.com', 'www.logertogo.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['157.180.127.70', 'logertogo.com', 'www.logertogo.com', 'localhost', '127.0.0.1']
 SITE_URL = 'https://logertogo.com'
 
 # Security & SSL Configuration (Senior Production Setup)
